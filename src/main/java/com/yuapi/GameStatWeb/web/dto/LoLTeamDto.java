650c235397621cdcfdc774c783eee536c326cce7
yuapi <phk2225@gmail.com>
2025-03-17 20:56:33 +0900

    Team DTO 작성.

---
+++ src/main/java/com/yuapi/GameStatWeb/web/dto/LoLTeamDto.java
@@ -0,0 +1,19 @@
+package com.yuapi.GameStatWeb.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class LoLTeamDto {
+    private String id;
+    private int tournamentId;
+    private String name;
+    private int iconId;
+    private int tier;
+    private String captain;
+    private String abbreviation;
+    private List<LoLPlayerDto> players;
+}
