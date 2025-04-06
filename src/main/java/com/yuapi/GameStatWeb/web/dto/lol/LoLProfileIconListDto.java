637c73793f92039c9a0e44b4af5d9d106700bd14
yuapi <phk2225@gmail.com>
2025-04-06 23:31:57 +0900

    ProfileIconList DTO 수정.

--- src/main/java/com/yuapi/GameStatWeb/web/dto/lol/LoLProfileIconListDto.java
+++ src/main/java/com/yuapi/GameStatWeb/web/dto/lol/LoLProfileIconListDto.java
@@ -3,14 +3,14 @@ package com.yuapi.GameStatWeb.web.dto.lol;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.List;
+import java.util.Map;
 
 @Getter
 @Builder
 public class LoLProfileIconListDto {
     private String type;
     private String version;
-    private List<IconData> data;
+    private Map<String, IconData> data;
 
     @Getter
     @Builder
