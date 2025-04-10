ff6469bf9d15ac83cc5576af9f7630623e16ba55
yuapi <phk2225@gmail.com>
2025-04-10 23:58:31 +0900

    예제 파일 일부 삭제.

--- src/main/java/com/yuapi/GameStatWeb/web/IndexController.java
+++ src/main/java/com/yuapi/GameStatWeb/web/IndexController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 @Controller
 public class IndexController {
 
-    private final PostsService postsService;
-
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("title", "GG Stats");
@@ -23,28 +21,28 @@ public class IndexController {
         return "index";
     }
 
-    @GetMapping("/example")
-    public String example(Model model) {
-        model.addAttribute("title", "Example Page");
-        model.addAttribute("posts", postsService.findAllDesc());
-
-        return "example";
-    }
-
-    @GetMapping("/posts/save")
-    public String postsSave(Model model) {
-        model.addAttribute("title", "Posts Save");
-
-        return "posts-save";
-    }
-
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model) {
-        model.addAttribute("title", "Posts Update");
-
-        PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post", dto);
-
-        return "posts-update";
-    }
+//    @GetMapping("/example")
+//    public String example(Model model) {
+//        model.addAttribute("title", "Example Page");
+//        model.addAttribute("posts", postsService.findAllDesc());
+//
+//        return "example";
+//    }
+//
+//    @GetMapping("/posts/save")
+//    public String postsSave(Model model) {
+//        model.addAttribute("title", "Posts Save");
+//
+//        return "posts-save";
+//    }
+//
+//    @GetMapping("/posts/update/{id}")
+//    public String postsUpdate(@PathVariable Long id, Model model) {
+//        model.addAttribute("title", "Posts Update");
+//
+//        PostsResponseDto dto = postsService.findById(id);
+//        model.addAttribute("post", dto);
+//
+//        return "posts-update";
+//    }
 }
